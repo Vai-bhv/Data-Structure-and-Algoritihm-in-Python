084806899ee4b60f4c4adb0a84167c6526a99bea
Vai-bhv <swciup13@gmail.com>
2023-01-22 20:48:22 +0100

    init

---
+++ adjust_matrix.py
@@ -0,0 +1,14 @@
+n = int(input())
+m = int(input())
+matrix = [[0] * n] * m
+lowest_value = matrix[0][0]
+for i in n:
+    pass
+    for j in m:
+        pass
+        if lowest_value < matrix[i][j]:
+            lowest_value = matrix[i][j]
+for i in n:
+    pass
+    for j in m:
+        matrix[i][j] -= lowest_value
